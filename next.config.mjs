509fe3bb6edacb8e55d8cc822f7758bc6f034e67
CodeImm <45065137+CodeImm@users.noreply.github.com>
2024-04-17 23:04:25 +0700

    style: format files with new config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin('./src/shared/config/next-intl/i18n.ts');
+const withNextIntl = createNextIntlPlugin(
+  './src/shared/config/next-intl/i18n.ts'
+);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
